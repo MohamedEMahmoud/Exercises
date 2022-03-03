e2960f040835c6f38eaf933766e7d32746728133
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-03-03 21:06:32 +0200

    Update show.ts

--- auth/src/routes/coach/show.ts
+++ auth/src/routes/coach/show.ts
@@ -6,13 +6,14 @@ const router = express.Router();
 
 router.get("/api/auth/coach",
     async (req: Request, res: Response) => {
+
         const coaches = await User.find({ role: RoleType.Coach });
 
         if (coaches.length === 0) {
             throw new BadRequestError("No Coaches found");
         }
 
-        res.status(200).send({ status: 200, coaches: coaches.map(coach => coach.coachName), success: true });
+        res.status(200).send({ status: 200, coaches: coaches.filter(coach => coach.coachName), success: true });
 
     });
 export { router as showCoachesName };
